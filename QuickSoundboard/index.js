3876c3fa0120723d890b4624bfdc32edbce58131
g5-j <g5-j@users.noreply.github.com>
2026-09-22 23:35:25 +0000

    deploy: 74f0b1d217c82fc7f4e83c4cdf616028a1d70b5a

--- QuickSoundboard/index.js
+++ QuickSoundboard/index.js
@@ -1,61 +1,39 @@
-var QuickSoundboard = (() => {
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
-    get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
-  }) : x)(function(x) {
-    if (typeof require !== "undefined") return require.apply(this, arguments);
-    throw Error('Dynamic require of "' + x + '" is not supported');
-  });
-  var __commonJS = (cb, mod) => function __require2() {
-    try {
-      return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
-    } catch (e) {
-      throw mod = 0, e;
-    }
-  };
-
-  // QuickSoundboard/src/index.ts
-  var require_index = __commonJS({
-    "QuickSoundboard/src/index.ts"(exports, module) {
-      var import_metro = __require("@vendetta/metro");
-      var import_common = __require("@vendetta/metro/common");
-      var import_plugin = __require("@vendetta/plugin");
-      var import_vendetta = __require("@vendetta");
-      var typedStorage = import_plugin.storage;
-      function playSound(sound) {
-        try {
-          const VoiceStateStore = (0, import_metro.findByProps)("getVoiceChannelId");
-          const ChannelStore = (0, import_metro.findByProps)("getChannel");
-          const currentChannelId = VoiceStateStore?.getVoiceChannelId();
-          if (!currentChannelId) return false;
-          const channel = ChannelStore?.getChannel(currentChannelId);
-          const guildId = channel?.guild_id ?? sound.guildId ?? "0";
-          import_common.FluxDispatcher.dispatch({
-            type: "GUILD_SOUNDBOARD_SOUND_PLAY_START",
-            soundId: sound.soundId,
-            channelId: currentChannelId,
-            guildId
-          });
-          return true;
-        } catch (e) {
-          import_vendetta.logger.error("[Quick Soundboard] Failed to play sound:", e);
-          return false;
-        }
-      }
-      function onLoad() {
-        import_vendetta.logger.log("[Quick Soundboard] Plugin Loaded Successfully.");
-        typedStorage.enabled ??= true;
-        typedStorage.favorites ??= [];
-      }
-      function onUnload() {
-        import_vendetta.logger.log("[Quick Soundboard] Plugin Unloaded.");
-      }
-      module.exports = {
-        onLoad,
-        onUnload,
-        playSound
-      };
-    }
-  });
-  return require_index();
-})();
+// QuickSoundboard/src/index.ts
+var import_metro = require("@vendetta/metro");
+var import_common = require("@vendetta/metro/common");
+var import_plugin = require("@vendetta/plugin");
+var import_vendetta = require("@vendetta");
+var typedStorage = import_plugin.storage;
+function playSound(sound) {
+  try {
+    const VoiceStateStore = (0, import_metro.findByProps)("getVoiceChannelId");
+    const ChannelStore = (0, import_metro.findByProps)("getChannel");
+    const currentChannelId = VoiceStateStore?.getVoiceChannelId();
+    if (!currentChannelId) return false;
+    const channel = ChannelStore?.getChannel(currentChannelId);
+    const guildId = channel?.guild_id ?? sound.guildId ?? "0";
+    import_common.FluxDispatcher.dispatch({
+      type: "GUILD_SOUNDBOARD_SOUND_PLAY_START",
+      soundId: sound.soundId,
+      channelId: currentChannelId,
+      guildId
+    });
+    return true;
+  } catch (e) {
+    import_vendetta.logger.error("[Quick Soundboard] Failed to play sound:", e);
+    return false;
+  }
+}
+function onLoad() {
+  import_vendetta.logger.log("[Quick Soundboard] Plugin Loaded Successfully.");
+  typedStorage.enabled ??= true;
+  typedStorage.favorites ??= [];
+}
+function onUnload() {
+  import_vendetta.logger.log("[Quick Soundboard] Plugin Unloaded.");
+}
+module.exports = {
+  onLoad,
+  onUnload,
+  playSound
+};
